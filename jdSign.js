4d0c18ec49a0b041b6d6e6e8f08e9ec9a4a06fdb
Daniel <daniel@funmobig.com>
2020-10-12 17:37:43 +0800

    [M]随机执行时间

--- jdSign.js
+++ jdSign.js
@@ -76,4 +76,42 @@ async function start() {
   }
 }
 
-start();
+function formatSeconds(value) { 
+  var theTime = parseInt(value);// 需要转换的时间秒 
+  var theTime1 = 0;// 分 
+  var theTime2 = 0;// 小时 
+  var theTime3 = 0;// 天
+  if(theTime > 60) { 
+      theTime1 = parseInt(theTime/60); 
+      theTime = parseInt(theTime%60); 
+      if(theTime1 > 60) { 
+          theTime2 = parseInt(theTime1/60); 
+          theTime1 = parseInt(theTime1%60); 
+          if(theTime2 > 24){
+              //大于24小时
+              theTime3 = parseInt(theTime2/24);
+              theTime2 = parseInt(theTime2%24);
+          }
+      } 
+  } 
+  var result = '';
+  if(theTime > 0){
+      result = ""+parseInt(theTime)+"秒";
+  }
+  if(theTime1 > 0) { 
+      result = ""+parseInt(theTime1)+"分"+result; 
+  } 
+  if(theTime2 > 0) { 
+      result = ""+parseInt(theTime2)+"小时"+result; 
+  } 
+  if(theTime3 > 0) { 
+      result = ""+parseInt(theTime3)+"天"+result; 
+  }
+  return result; 
+} 
+
+var time = Math.random() * 1000 * 60 * 60 * 3
+console.log(formatSeconds(time / 1000) + "后开始执行")
+setTimeout(() => {
+  start()
+}, time);

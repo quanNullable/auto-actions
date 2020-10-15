77f5c93d16f4f7b889b8621c144ac7d857334a47
Daniel <daniel@funmobig.com>
2020-10-15 22:25:06 +0800

    123

--- python/app.py
+++ python/app.py
@@ -1,12 +1,11 @@
 from Blinker import Blinker, BlinkerButton
 import os
 
-buttonRestart = BlinkerButton("btn-restart")
+blinkerId = os.environ["BLINKER_ID"]
+Blinker.mode("BLINKER_WIFI")
+Blinker.begin(blinkerId)
 
-def init():
-    blinkerId = os.environ["BLINKER_ID"]
-    Blinker.mode("BLINKER_WIFI")
-    Blinker.begin(blinkerId)
+buttonRestart = BlinkerButton("btn-restart")
 
 def restart_callback(state):
     buttonRestart.print(state)
@@ -16,12 +15,9 @@ def restart_callback(state):
 def data_callback(data):
     Blinker.print('收到:'+data)
 
-def start():
-    buttonRestart.attach(restart_callback)
-    Blinker.attachData(data_callback)
+buttonRestart.attach(restart_callback)
+Blinker.attachData(data_callback)
 
 if __name__ == '__main__':
-    init()
-    start()
     while True:
         Blinker.run()

cfece4c786d0e52b0c2356b27bf43f5e603b9e2f
Daniel <daniel@funmobig.com>
2020-10-16 17:50:44 +0800

    [F]添加捕获异常

--- python/blinker/controllers/executor.py
+++ python/blinker/controllers/executor.py
@@ -10,24 +10,28 @@ from tools.ip import getIPs
 from tools.notice import sendTextMsg
 import task
 
+
 def executCommand(command):
-    if command.Name == ALL_COMANDS[0].Name: #获取ip
-        result = getIPs()
-    elif command.Name == ALL_COMANDS[1].Name: #执行代码
-        threading.Thread(
-            target=_executeShell,
-            args=(command.Parmas,)).start()
-        result = '已开始执行'
-    elif command.Name == ALL_COMANDS[2].Name:  #命令帮助
-        result = _getCommandsHelp()
-    elif command.Name == ALL_COMANDS[3].Name:  #任务详情
-        result = task.getJobs()
-    elif command.Name == ALL_COMANDS[4].Name:  #立即执行任务
-        result = _runTaskRightNow()
-    else:
-        result = '暂未完成'
-    Logger.v('命令<' + command.Name + '>执行结果<' + str(result) + '>')
-    return result
+    try:
+        if command.Name == ALL_COMANDS[0].Name:  #获取ip
+            result = getIPs()
+        elif command.Name == ALL_COMANDS[1].Name:  #执行代码
+            threading.Thread(
+                target=_executeShell, args=(command.Parmas, )).start()
+            result = '已开始执行'
+        elif command.Name == ALL_COMANDS[2].Name:  #命令帮助
+            result = _getCommandsHelp()
+        elif command.Name == ALL_COMANDS[3].Name:  #任务详情
+            result = task.getJobs()
+        elif command.Name == ALL_COMANDS[4].Name:  #立即执行任务
+            result = _runTaskRightNow()
+        else:
+            result = '暂未完成'
+        Logger.v('命令<' + command.Name + '>执行结果<' + str(result) + '>')
+        return result
+    except Exception as e:
+        Logger.e('命令<' + command.Name + '>执行失败', e)
+        return '内部错误,执行失败'
 
 
 def _executeShell(command):
@@ -37,7 +41,7 @@ def _executeShell(command):
 
 
 def _runTaskRightNow(funcName):
-    func = getattr(task, funcName,None)
+    func = getattr(task, funcName, None)
     if func is None:
         sendTextMsg('未找到指定任务')
     else:
@@ -45,7 +49,8 @@ def _runTaskRightNow(funcName):
             Logger.v('开始执行' + funcName)
             func(True)
         else:
-            Logger.e(func + '无法执行','not callable')
+            Logger.e(func + '无法执行', 'not callable')
+
 
 def _getCommandsHelp():
     def formatCommands(commands):
@@ -55,15 +60,17 @@ def _getCommandsHelp():
             '作用:'+comand.Func+'\n'+\
             '用法:'+comand.Usage+'\n'
             descs.append(desc)
-        return '\n'.join(descs)  
+        return '\n'.join(descs)
+
     commandsCount = len(ALL_COMANDS)
     if commandsCount == 0:
         return '暂无可用命令'
     else:
         return formatCommands(ALL_COMANDS)
 
+
 def _runTaskRightNow(user, funcName):
-    func = getattr(task, funcName,None)
+    func = getattr(task, funcName, None)
     if func is None:
         sendTextMsg(user.Id, '未找到指定任务')
     else:
@@ -71,4 +78,4 @@ def _runTaskRightNow(user, funcName):
             Logger.v('开始执行' + funcName)
             func(True)
         else:
-            Logger.e(func + '无法执行','not callable')
+            Logger.e(func + '无法执行', 'not callable')
